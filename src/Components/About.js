084b10df621673c11c10f3d1694052c495dcc3f1
sanoofp <sanoofpb24@gmail.com>
2017-12-03 09:27:45 -0800

    Updated README.md

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,6 +3,7 @@ import React from 'react';
 const About = (props) => {
     return(
         <div className="about">
+          <h5>v1.2.0</h5>
           <p>Developed By <a rel="noopener noreferrer" target="_blank" href="https://github.com/sanoofp">Sanoof pandikkadan</a></p>
           <div className="about-user">
             <a href="/"><i className="fa fa-facebook fa-fw"></i></a>		
