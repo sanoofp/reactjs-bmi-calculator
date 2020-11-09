e7f6fdf41f0f300d5cb09ca3636d5d41595faa05
Sanoof Pandikkadan <sanoofp7@gmail.com>
2020-11-09 20:07:00 +0530

    Update About.js

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,7 +5,6 @@ const About = (props) => {
         <div className="about">
           <div>
             <h5>v1.2.0</h5>
-            <p>Developed By <a rel="noopener noreferrer" target="_blank" href="https://github.com/sanoofp">Sanoof pandikkadan</a></p>
           </div>
           <div className="about-user">
             <a href="/"><i className="fa fa-facebook fa-fw"></i></a>		
